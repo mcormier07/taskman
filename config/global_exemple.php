58a33ab49a88db2d7952cf6be5e624e4cdf4fd3d
mcormier07 <mcormier.pro@gmail.com>
2022-09-23 14:59:42 +0200

    added exemple file to setup database

---
+++ config/global_exemple.php
@@ -0,0 +1,5 @@
+<?php
+define('DB_NAME', ''); # Here goes the database name
+define('DB_HOST', ''); # Here goes the database server you want to connect
+define('DB_USER', ''); # Here goes the database user (user has to be created before)
+define('DB_PASS', ''); # Here goes the password of the user created (can be empty, but not recommanded);
